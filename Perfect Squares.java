59da3c3ebb9dd1d41156888c058efb657f941565
Sri Saravanan <srisaravanan505@gmail.com>
2026-07-20 19:41:36 +0530

    Create Perfect Squares.java

---
+++ Perfect Squares.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+        int d[]=new int[n+1];
+        d[0]=0;
+        for(int i=1;i<=n;i++){
+            d[i]=n;
+            for(int j=1;j*j<=i;j++){
+                d[i]=Math.min(d[i],d[i-j*j]+1);
+            }
+        }
+      return d[n];
+    }
+}
