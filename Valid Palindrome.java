51f3d4227f6239b7605c165b05419135440b7b02
Sri Saravanan <srisaravanan505@gmail.com>
2026-07-20 19:16:30 +0530

    Create  Valid Palindrome.java

---
+++ Valid Palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]" , "").toLowerCase();
+        char c[]=str.toCharArray();
+        int l=0;
+        int r=c.length();
+        while(l<r){
+            
+        }
+    }
+}
